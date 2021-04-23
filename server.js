ec62557767a033672ecdafadbe8585dde18385be
TheRightClick <neversickclick@gmail.com>
2021-04-22 20:43:01 -0400

    fixed url in cors

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ db.on('disconnected', ()=> console.log('mongoose disconnected'));
 app.use(express.json());
 
 // cors middleware
-const whitelist = ['http://localhost:3000', 'https://git.heroku.com/p3sandboxfrontend.git']
+const whitelist = ['http://localhost:3000', 'https://p3sandboxreact.herokuapp.com']
 const corsOptions = {
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
