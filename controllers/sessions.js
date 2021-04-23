51f405d29258e62373f8f12a274204d49e24fba3
TheRightClick <neversickclick@gmail.com>
2021-04-22 21:33:28 -0400

    fix delete route

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -69,7 +69,7 @@ router.post('/', (req, res) => {
 })
 
 
-router.delete('/signout', (req, res) => {
+router.delete('/', (req, res) => {
     User.findOne({ username: req.body.username}, (err, foundUser) => {
         if (err) {
                 res.send(err)
